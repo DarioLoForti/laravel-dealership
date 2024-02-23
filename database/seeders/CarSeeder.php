ec7e20cc88e34c56be784d897a900ade7e998c54
Zariy7 <nazariy.hereha@gmail.com>
2024-02-23 16:05:48 +0100

    car seeder functional

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Car as Car;
+use Faker\Generator as Faker;
 
 class CarSeeder extends Seeder
 {
@@ -12,8 +14,27 @@ class CarSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<20; $i++){
+            $newCar = new Car;
+
+            $newCar->marca=$faker->word();
+            $newCar->modello=$faker->words(2, true);
+            $newCar->prezzo=$faker->randomFloat(2, 5, 50);
+            $newCar->alimentazione=$faker->randomElement(['Diesel', 'Benzina', 'Elettrica', 'GPL', 'Batteria', 'Ibrido']);
+            $newCar->cilindrata=10*$faker->numberBetween(15, 1000);
+            $newCar->cambio=$faker->randomElement(['Manuale', 'Automatico']);
+            $newCar->porte=$faker->randomElement(['2', '4']);
+            $newCar->posti=$faker->randomElement(['2', '4', '6', '8']);
+            $newCar->tipologia=$faker->randomElement(['SUV', 'Utilitaria', 'Berlina', 'Station Wagon', 'Coupé', 'Van', 'Monovolume', 'Cabriolet']);
+            $newCar->stato=$faker->randomElement(['nuovo', 'usato']);
+            $newCar->anno_immatricolazione=$faker->dateTimeBetween('-10 year', '0 year');
+            $newCar->km=1000*$faker->numberBetween(10, 1000);
+            $names=explode(' ', $newCar->modello);
+            $newCar->immagine=$faker->imageUrl(360, 360, $names[0], true, $names[1], true);
+
+            $newCar->save();
+        }
     }
 }
