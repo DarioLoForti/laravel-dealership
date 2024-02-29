4926ae1cb1a29be29f014a1a5033f6db87a44c39
DarioLoForti <lofortidario@gmail.com>
2024-02-29 17:36:55 +0100

    destroy brand

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -95,6 +95,7 @@ class BrandController extends Controller
      */
     public function destroy(Brand $brand)
     {
-        //
+        $brand->delete();
+        return redirect()->route('admin.brands.index');
     }
 }
