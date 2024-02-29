afafd8b478d7f8aa253ddb10aaba161f83907adb
Zariy7 <nazariy.hereha@gmail.com>
2024-02-29 16:29:30 +0100

    request rules fixed

--- app/Http/Requests/StoreBrandRequest.php
+++ app/Http/Requests/StoreBrandRequest.php
@@ -24,30 +24,35 @@ class StoreBrandRequest extends FormRequest
     public function rules()
     {
         return [
-            'nome' => 'required|min:3|max:30',
+            'nome' => 'required|min:3|max:50|unique',
             'indirizzo' => 'required',
-            'email' => 'required|email',
+            'email' => 'required|email|max:150',
             'sito_web' => 'required',
-            'telefono' => 'required|min:3|max:20',
-            'tipologia' => 'required|min:3|max:30',
-            'nazione' => 'required|min:3|max:30',
+            'telefono' => 'required|min:3|max:50',
+            'tipologia' => 'required|min:3|max:150',
+            'nazione' => 'required|min:3|max:150',
         ];
     }
-
+    
     public function messages()
     {
         return [
             'nome.required' => 'Il nome è obbligatorio!',
             'nome.min' => 'Il nome non può essere più corto di 3 caratteri.',
-            'nome.max' => 'Il nome non può essere più lungo di 30 caratteri.',
+            'nome.max' => 'Il nome non può essere più lungo di 150 caratteri.',
+            'nome.unique' => 'Il nome deve essere univoco!',
             'email.required' => 'La mail è obbligatoria!',
             'email.email' => 'La mail deve essere in formato email! (Contenere un @ e un indirizzo valido dopo.)',
+            'email.max' => 'La mail non può essere più lunga di 150 caratteri.',
             'sito_web.required' => 'Il sito web è obbligatorio!',
             'sito_web.min' => 'Il sito web non può essere più corto di 3 caratteri.',
-            'sito_web.max' => 'Il sito web non può essere più lungo di 30 caratteri.',
+            'sito_web.max' => 'Il sito web non può essere più lungo di 150 caratteri.',
+            'telefono.required' => 'Il numero di telefono è obbligatorio!',
+            'telefono.min' => 'Il numero di telefono non può essere più corto di 3 caratteri.',
+            'telefono.max' => 'Il numero di telefono non può essere più lungo di 150 caratteri.',
             'nazione.required' => 'La nazione è obbligatorio!',
             'nazione.min' => 'La nazione non può essere più corto di 3 caratteri.',
-            'nazione.max' => 'La nazione non può essere più lungo di 30 caratteri.',
+            'nazione.max' => 'La nazione non può essere più lungo di 150 caratteri.',
         ];
     }
 }
