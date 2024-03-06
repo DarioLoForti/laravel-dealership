d5d54e008510631425cc7024a9f4fd808774f1d2
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-03-06 11:51:08 +0100

    last-update

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\CarController as CarController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/cars', [CarController::class, 'index']);
+
+Route::get('/car/{id}', [CarController::class, 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
